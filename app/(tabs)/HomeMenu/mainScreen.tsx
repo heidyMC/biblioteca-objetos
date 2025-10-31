3838eaac4b791194bbb929d19144d95da976dea7
Heidy <heidy20@gmail.com>
2025-10-31 13:37:14 -0400

    feat(main): imports base (UI, supabase, router, AsyncStorage)

--- app/(tabs)/HomeMenu/mainScreen.tsx
+++ app/(tabs)/HomeMenu/mainScreen.tsx
@@ -1,5 +1,21 @@
-import React from "react"
-import { ScrollView } from "react-native"
+"use client"
+
+import TextComponent from "@/components/ui/text-component"
+import { supabase } from "@/lib/supabase"
+import { Ionicons } from "@expo/vector-icons"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { useFocusEffect, useRouter } from "expo-router"
+import { useCallback, useEffect, useState } from "react"
+import {
+  ActivityIndicator,
+  Image,
+  Modal,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native"
 
 export default function MainScreen() {
   return <ScrollView />
